32ff1e42e9643537f62c0bde5dfb6fca47ef7421
CKdave <chirag.django@gmail.com>
2020-09-19 17:42:10 +0530

    third commit

--- blogsite2/settings.py
+++ blogsite2/settings.py
@@ -25,9 +25,9 @@ STATIC_DIR = os.path.join(BASE_DIR,'static')
 SECRET_KEY = 'ddx^6!+ialu5!hl$by(^kl)+$)uq%bvszhe_%85=)^@8kt^ufc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
